34fdeea93ad50fa184b9f6da5f0f1301283e8da1
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-09-02 07:28:42 +0400

    db: version embedded migrations

--- db/migrations/embed.go
+++ db/migrations/embed.go
@@ -2,10 +2,27 @@ package migrations
 
 import _ "embed"
 
+type Migration struct {
+	Version int
+	Up      string
+	Down    string
+}
+
 //go:embed 000001_initial.up.sql
-var Up string
+var initialUp string
 
 //go:embed 000001_initial.down.sql
-var Down string
+var initialDown string
 
-const LatestVersion = 1
+//go:embed 000002_outbox_idempotency.up.sql
+var outboxIdempotencyUp string
+
+//go:embed 000002_outbox_idempotency.down.sql
+var outboxIdempotencyDown string
+
+var All = []Migration{
+	{Version: 1, Up: initialUp, Down: initialDown},
+	{Version: 2, Up: outboxIdempotencyUp, Down: outboxIdempotencyDown},
+}
+
+const LatestVersion = 2
